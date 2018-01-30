47ef1d2e90b9befce137e04de8f850e853f28a2b
Nutti Saelor <leelorz6@gmail.com>
2018-01-30 17:27:03 +0700

    add new state to reducer

--- src/app/shopping-list/store/shopping-list.reducers.ts
+++ src/app/shopping-list/store/shopping-list.reducers.ts
@@ -1,8 +1,20 @@
 import { Ingredient } from './../../shared/ingredient.model';
 import * as ShoppingListActions from './shopping-list.actions';
 
+export interface AppState {
+  shoppingList: State;
+}
+
+export interface State {
+  ingredients: Ingredient[];
+  editedIngredient: Ingredient;
+  editedIngredientIndex: number;
+}
+
 const initialState = {
-  ingredients: [new Ingredient('Apples', 5), new Ingredient('Tomatoes', 10)]
+  ingredients: [new Ingredient('Apples', 5), new Ingredient('Tomatoes', 10)],
+  editedIngredient: null,
+  editedIngredientIndex: -1
 };
 
 export function shoppingListReducer(
@@ -19,6 +31,25 @@ export function shoppingListReducer(
       return {
         ...state,
         ingredients: [...state.ingredients, ...action.payload]
+      };
+    case ShoppingListActions.UPDATE_INGREDIENT:
+      const ingredient = state.ingredients[action.payload.index];
+      const updatedIngredient = {
+        ...ingredient,
+        ...action.payload.ingredient
+      };
+      const ingredients = [...state.ingredients];
+      ingredients[action.payload.index] = updatedIngredient;
+      return {
+        ...state,
+        ingredients: ingredients
+      };
+    case ShoppingListActions.DELETE_INGREDIENT:
+      const oldIngredients = [...state.ingredients];
+      oldIngredients.splice(action.payload, 1);
+      return {
+        ...state,
+        ingredients: oldIngredients
       };
     default:
       return state;
