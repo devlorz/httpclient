461fb4c6d68785fde19a47f388c7f53d52a57c90
Nutti Saelor <leelorz6@gmail.com>
2018-02-19 17:26:06 +0700

    add authSignin and authLogout effects

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -1,6 +1,8 @@
+import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { Effect, Actions } from '@ngrx/effects';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/switchMap';
 import { fromPromise } from 'rxjs/observable/fromPromise';
 import * as firebase from 'firebase';
@@ -37,5 +39,39 @@ export class AuthEffects {
       ];
     });
 
-  constructor(private actions$: Actions) {}
+  @Effect()
+  authSignin = this.actions$
+    .ofType(AuthActions.TRY_SIGNIN)
+    .map((action: AuthActions.TrySignin) => {
+      return action.payload;
+    })
+    .switchMap((authData: { username: string; password: string }) => {
+      return fromPromise(
+        firebase
+          .auth()
+          .signInWithEmailAndPassword(authData.username, authData.password)
+      );
+    })
+    .switchMap(() => {
+      return fromPromise(firebase.auth().currentUser.getIdToken());
+    })
+    .mergeMap((token: string) => {
+      this.router.navigate(['/']);
+      return [
+        {
+          type: AuthActions.SIGNIN
+        },
+        {
+          type: AuthActions.SET_TOKEN,
+          payload: token
+        }
+      ];
+    });
+
+  @Effect({ dispatch: false })
+  authLogout = this.actions$.ofType(AuthActions.LOGOUT).do(() => {
+    this.router.navigate(['/']);
+  });
+
+  constructor(private actions$: Actions, private router: Router) {}
 }
